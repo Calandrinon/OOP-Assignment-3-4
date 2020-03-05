34819e8e10261beeda707f0cd6bc0f9cd0b8bf29
George <georgesut@yahoo.com>
2020-03-05 17:55:18 +0200

    Add exit command

--- ui.c
+++ ui.c
@@ -18,14 +18,22 @@ void ui_exit(UI* ui) {
 
 
 void run(UI* ui) {
-    char* commands[] = {"exit"};
-    void (*command_functions[])() = {ui_exit};
+    char* commands[] = {"exit\0"};
+    void (*command_functions[])(UI*) = {ui_exit};
     char command[40];
+
+    int number_of_commands = sizeof(commands)/sizeof(commands[0]);
+    printf("%d\n", number_of_commands);
 
     while (ui->running) {
         fgets(command, 40, stdin);
+        int found = 0;
 
-        if (strncmp(command, "exit", 4) == 0)
-            ui_exit(ui);
+        for (int i = 0; i < number_of_commands && !found; i++) {
+            if (strncmp(command, commands[i], strlen(commands[i])) == 0) {
+                found = 1;
+                command_functions[i](ui);
+            }
+        }
     }
 }

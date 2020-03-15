1058080f58dd2c846c00f8a99a1af6aa867f00f0
George <georgesut@yahoo.com>
2020-03-15 19:43:09 +0200

    Shorten build scripts

--- compile.sh
+++ compile.sh
@@ -1,8 +1,1 @@
-gcc -Wall -g -c main.c
-gcc -Wall -g -c signal.c
-gcc -Wall -g -c repository.c
-gcc -Wall -g -c service.c
-gcc -Wall -g -c ui.c
-gcc -Wall -g -c tests.c
-gcc -Wall -Werror -g -O0 -o main *.o
-
+gcc -Wall -g -o main *.c
--- script_test_signal.sh
+++ script_test_signal.sh
@@ -1,10 +1,3 @@
-gcc -Wall -c main.c
-gcc -Wall -c signal.c
-gcc -Wall -c repository.c
-gcc -Wall -c service.c
-gcc -Wall -c ui.c
-gcc -Wall -c tests.c
-gcc -Wall -g -o main *.o
+./compile.sh
 ./main
-rm *.o
 rm main

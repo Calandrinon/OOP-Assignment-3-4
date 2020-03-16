919e9e0b1e983c4d3354fb20baccd4f9c907b539
George <georgesut@yahoo.com>
2020-03-16 08:08:05 +0200

    Add specifications

--- repository.c
+++ repository.c
@@ -4,6 +4,14 @@
 #include <string.h>
 
 SignalRepository create_repository() {
+    /**
+        Creates a repository and initializes all the members of the struct SignalRepository.
+        Returns the created repository.
+
+        Output:
+            - a struct of type Repository
+    **/
+
     SignalRepository repository;
     repository.container.number_of_elements = 0;
     repository.container.array_size = 2;
@@ -14,6 +22,17 @@ SignalRepository create_repository() {
 
 
 void add_signal(SignalRepository *repository, int id, char modulated_signal[], char type[], int priority_number) {
+    /**
+        Adds a signal with the given parameters to the repository.
+
+        Input:
+            - repository: a pointer to a Repository struct
+            - id: int
+            - modulated_signal: string
+            - type: string
+            - priority_number: int
+    **/
+
     if (repository->container.number_of_elements == repository->container.array_size) {
         int new_signals_array_size = repository->container.array_size * 2;
         Signal* new_signals_array = (Signal*)malloc(new_signals_array_size*sizeof(Signal));
@@ -25,7 +44,6 @@ void add_signal(SignalRepository *repository, int id, char modulated_signal[], c
         repository->container.array_size = new_signals_array_size;
         free(repository->container.signals);
         repository->container.signals = new_signals_array;
-        //printf("New size of the repository container: %d\n", new_signals_array_size);
     }
 
     for (int i = 0; i < repository->container.number_of_elements; i++) {
@@ -42,6 +60,14 @@ void add_signal(SignalRepository *repository, int id, char modulated_signal[], c
 
 
 void delete_signal_by_id(SignalRepository *repository, int signal_id) {
+    /**
+        Deletes a signal with the given id from the repository.
+
+        Input:
+            - repository: a pointer to a Repository struct
+            - id: int
+    **/
+
     int id_found = 0;
 
     for (int i = 0; i < repository->container.number_of_elements; i++) {
@@ -55,11 +81,24 @@ void delete_signal_by_id(SignalRepository *repository, int signal_id) {
     if (id_found)
         repository->container.number_of_elements--;
     else
-        printf("No!\n");
+        printf("No!\n"); // There's no exceptions in C.
+                         // Maybe I should have bothered to return a boolean value in this case
+                         // instead of printing from the repository.
 }
 
 
 void update_signal(SignalRepository *repository, int id, char new_modulated_signal[], char new_type[], int new_priority_number) {
+    /**
+        Updates a signal with the given parameters from the repository.
+
+        Input:
+            - repository: a pointer to a Repository struct
+            - id: int
+            - new_modulated_signal: string
+            - new_type: string
+            - new_priority_number: int
+    **/
+
     for (int i = 0; i < repository->container.number_of_elements; i++) {
         if (repository->container.signals[i].id == id) {
             set_modulated_signal(&(repository->container.signals[i]), new_modulated_signal);
@@ -72,6 +111,21 @@ void update_signal(SignalRepository *repository, int id, char new_modulated_sign
 
 
 Signal search_signal(SignalRepository *repository, int signal_id) {
+    /**
+        Searches a signal with the given signal_id and returns it
+        from the repository in case it exists. Otherwise, it will return
+        a dummy Signal struct with all values set to -1.
+
+        Input:
+            - repository: a pointer to a Repository struct
+            - signal_id: int
+
+        Output:
+            - signal: the searched signal with id "signal_id"
+            OR:
+            - a signal with the values set to -1, in case a signal with id
+            "signal_id" is not found
+    **/
 
     for (int i = 0; i < repository->container.number_of_elements; i++) {
         if (repository->container.signals[i].id == signal_id)
@@ -84,15 +138,41 @@ Signal search_signal(SignalRepository *repository, int signal_id) {
 
 
 SignalContainer get_signal_container(const SignalRepository *repository) {
+    /**
+        Gets the container associated with the repository pointed to by
+        "const SignalRepository* repository".
+
+        Input:
+            - repository: a pointer to a Repository struct
+
+        Output:
+            - container: the container associated with the repository
+    **/
+
     return repository->container;
 }
 
 
 void free_repository(SignalRepository* repository) {
+    /**
+        Frees the dynamically allocated resources used by the struct pointed to by SignalRepository* repository.
+
+        Input:
+            - repository: a pointer to the SignalRepository struct
+    **/
+
     free(repository->container.signals);
 }
 
 UndoStack create_undo_stack() {
+    /**
+        Creates a stack and initializes all the members of the struct UndoStack.
+        Returns the created repository.
+
+        Output:
+            - a struct of type UndoStack
+    **/
+
     UndoStack undo_stack;
     undo_stack.number_of_elements = 0;
     undo_stack.array_size = 4;
@@ -107,6 +187,14 @@ UndoStack create_undo_stack() {
 
 
 void push_command(UndoStack* undo_stack, char* command) {
+    /**
+        Pushes the command "command" onto the stack pointed to by "undo_stack"
+
+        Input:
+            - undo_stack: a pointer to an UndoStack struct
+            - command: string
+    **/
+
     if (undo_stack->number_of_elements == undo_stack->array_size) {
         int new_array_size = undo_stack->array_size * 2;
         char** new_array = (char**)malloc(new_array_size*sizeof(char*));
@@ -134,6 +222,16 @@ void push_command(UndoStack* undo_stack, char* command) {
 
 
 char* pop_command(UndoStack* undo_stack) {
+    /**
+        Pops the command "command" from the stack pointed to by "undo_stack"
+
+        Input:
+            - undo_stack: a pointer to an UndoStack struct
+
+        Output:
+            - last_command: the element from the top of the command stack
+    **/
+
     int last_element = undo_stack->number_of_elements - 1;
     char* last_command = undo_stack->commands[last_element];  //// POSSIBLE MEMORY LEAK
 
@@ -143,6 +241,13 @@ char* pop_command(UndoStack* undo_stack) {
 
 
 void free_undo_stack(UndoStack* undo_stack) {
+    /**
+        Frees the dynamically allocated resources used by the struct pointed to by UndoStack* undo_stack.
+
+        Input:
+            - undo_stack: a pointer to an UndoStack struct
+    **/
+
     for (int i = 0; i < undo_stack->array_size; i++) {
         free(undo_stack->commands[i]);
     }
--- service.c
+++ service.c
@@ -4,6 +4,17 @@
 #include <stdlib.h>
 
 Service create_service(SignalRepository* repository) {
+    /**
+        Creates a service and initializes all the members of the struct Service.
+        Returns the created service.
+
+        Input:
+            - service: a pointer to the repository associated with the service
+
+        Output:
+            - a struct of type Service
+    **/
+
     Service service;
     service.repository = repository;
     service.undo_stack = create_undo_stack();
@@ -13,11 +24,32 @@ Service create_service(SignalRepository* repository) {
 
 
 void service_add(Service* service, int id, char modulated_signal[], char type[], int priority_number) {
+    /**
+        Adds a signal with the given parameters into the repository with the
+        function "add_signal".
+
+        Input:
+            - service: a pointer to the Service struct
+            - id: int
+            - modulated_signal: string
+            - type: string
+            - priority_number: int
+    **/
+
     add_signal(service->repository, id, modulated_signal, type, priority_number);
 }
 
 
 void service_delete(Service* service, int id) {
+    /**
+        Deletes a signal with the id "id" from the repository with the
+        function "delete_signal_by_id".
+
+        Input:
+            - service: a pointer to the Service struct
+            - id: int
+    **/
+
     SignalRepository new_repository = *(service->repository);
     delete_signal_by_id(&new_repository, id);
     *(service->repository) = new_repository;
@@ -25,6 +57,18 @@ void service_delete(Service* service, int id) {
 
 
 void service_update(Service *service, int id, char new_modulated_signal[], char new_type[], int new_priority_number) {
+    /**
+        Updates a signal with the given id and new parameters into the repository with the
+        function "update_signal".
+
+        Input:
+            - service: a pointer to the Service struct
+            - id: int
+            - new_modulated_signal: string
+            - new_type: string
+            - new_priority_number: int
+    **/
+
     SignalRepository new_repository = *(service->repository);
     update_signal(&new_repository, id, new_modulated_signal, new_type, new_priority_number);
     *(service->repository) = new_repository;
@@ -32,6 +76,16 @@ void service_update(Service *service, int id, char new_modulated_signal[], char 
 
 
 SignalContainer service_get_container(Service* service) {
+    /**
+        Gets the container from the repository.
+
+        Input:
+            - service: a pointer to the Service struct
+
+        Output:
+            - container: the desired container
+    **/
+
 	SignalRepository repository = *(service->repository);
 	SignalContainer container = get_signal_container(&repository);
 	return container;
@@ -39,6 +93,13 @@ SignalContainer service_get_container(Service* service) {
 
 
 void free_service(Service* service) {
+    /**
+        Frees the dynamically allocated resources used by the struct pointed to by Service* service.
+
+        Input:
+            - service: a pointer to the Service struct
+    **/
+
     free_repository(service->repository);
     free_undo_stack(&service->undo_stack);
     free_undo_stack(&service->redo_stack);
@@ -46,6 +107,17 @@ void free_service(Service* service) {
 
 
 void service_split_into_tokens(Service* service, char* command, char* tokens[]) {
+    /**
+        Splits a command into tokens.
+
+        Input:
+            - service: a pointer to the Service struct
+            - command: string
+
+        Output:
+            - tokens: the tokens obtained from the command
+    **/
+
     int token_index = 0;
     char* token = strtok(command, " ");
 
@@ -58,6 +130,18 @@ void service_split_into_tokens(Service* service, char* command, char* tokens[]) 
 
 
 char* service_get_reversed_command(Service* service, char* command) {
+    /**
+        Returns the reverse of a command.
+        Example: for "add 1 a a 1", it returns delete 1
+
+        Input:
+            - service: a pointer to the Service struct
+            - command: string
+
+        Output:
+            - reversed: the reversed command
+    **/
+
     char* tokens[5];
     char command_string[50];
     strcpy(command_string, command);
@@ -108,6 +192,15 @@ char* service_get_reversed_command(Service* service, char* command) {
 
 
 void service_push_last_command_on_stack(Service* service, char* last_command) {
+    /**
+        Pushes the last command that has been given on a stack of previously
+        executed commands.
+        Used for the undo feature.
+
+        Input:
+            - service: a pointer to the Service struct
+            - command: string
+    **/
     char* reversed_command = service_get_reversed_command(service, last_command);
 
     if (reversed_command == NULL)
@@ -121,6 +214,13 @@ void service_push_last_command_on_stack(Service* service, char* last_command) {
 
 
 void service_undo(Service* service) {
+    /**
+        Undoes the last executed command.
+
+        Input:
+            - service: a pointer to the Service struct
+    **/
+
     if (service->undo_stack.number_of_elements == 0)
         /// Validates the size of the stack before undoing,
         /// otherwise the program would issue a segmentation fault in the
@@ -147,6 +247,13 @@ void service_undo(Service* service) {
 
 
 void service_redo(Service* service) {
+    /**
+        Redoes the last undone command.
+
+        Input:
+            - service: a pointer to the Service struct
+    **/
+
     if (service->redo_stack.number_of_elements == 0)
         return;
 
